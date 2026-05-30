73bacb318590775cbef39de7ca6ea2356ea0c4e2
Yash Vimal <yashsarthak79@gmail.com>
2026-05-30 14:19:56 +0530

    serving_multiple_files

---
+++ 02_node_js/public/index.js
@@ -0,0 +1,3 @@
+const my_intro = document.getElementById('intro')
+
+my_intro.textContent = 'My name is Yash Vimal I am persuing BCA from SKIT College which is affliated from Chattrapati Shahuji Maharaj. My aim is to become a software engineer.'
--- 02_node_js/utils/serveStatic.js
+++ 02_node_js/utils/serveStatic.js
@@ -1,14 +1,27 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
 
 export async function baseDirectory(req, res, base){
-    const filePath = path.join(base, 'public', 'index.html')
-    
+            
+        const PublicDir = path.join(base, 'public')
+        const filePath = path.join(
+            PublicDir, 
+            req.url === '/' ? 'index.html' : req.url
+        )
+        const ext = path.extname(filePath)    
+        const contentType = getContentType(ext)
+
     try{
         const content = await fs.readFile(filePath)
-        sendResponse(res, 200, 'text/html', content)
+        sendResponse(res, 200, contentType, content)
     } catch (err) {
-        console.log(err)
+        if(err.code === 'ENOENT'){
+            const content = await fs.readFile(path.join(PublicDir, '404.html'))
+            sendResponse(res, 404, 'text/html', content)
+        } else {
+            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err.code}</h1></html>`)
+        }
     }
 }
